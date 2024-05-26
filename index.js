a11928203acb21580f03d17e962fc464ca8bbe47
IramKayani <kayani345@gmail.com>
2024-05-26 22:11:01 +0500

    adding word counter project

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr /bin /env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.bgGray("Welcome to Word Counter"));
+let answers = await inquirer.prompt([
+    {
+        name: "Q1",
+        Type: "input",
+        message: "Enter yur Sentence",
+    }
+]);
+let words = answers.Q1.trim().split(" ");
+console.log(words);
+console.log(`${words.length}`);
